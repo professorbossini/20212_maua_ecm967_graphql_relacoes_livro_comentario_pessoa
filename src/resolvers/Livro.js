36c691eee992fcddb6e97f8fd3562dc89daa4eca
Rodrigo Bossini <professorbossini@gmail.com>
2021-09-18 09:05:08 -0300

    Encerramos a apostila 05, refatorando a aplicacao

---
+++ src/resolvers/Livro.js
@@ -0,0 +1,11 @@
+const Livro = {
+    autor (parent, args, ctx, info){
+        return ctx.db.pessoas.find((pessoa) => pessoa.id === parent.autor)
+        //console.log (`Parent: ${JSON.stringify(parent)}`)
+    },
+    comentarios (parent, args, ctx, info){
+        return ctx.db.comentarios.filter (comentario => comentario.livro === parent.id)
+    }
+}
+
+export default Livro
---
+++ src/resolvers/Mutation.js
@@ -0,0 +1,75 @@
+
+import {
+    v4 as uuidv4
+} from 'uuid'
+
+const Mutation = {
+    inserirPessoa (parent, args, ctx, info){
+        const pessoa = {
+            id: uuidv4(),
+            nome: args.pessoa.nome,
+            idade: args.pessoa.idade
+        }
+        ctx.db.pessoas.push(pessoa)
+        return pessoa
+    },
+    removerPessoa (parent, args, ctx, info){
+        const indice = ctx.db.pessoas.findIndex(p => p.id === args.id)
+        if (indice < 0)
+            throw new Error ("Pessoa não existe")
+            //splice operar in-place
+        const removido = ctx.db.pessoas.splice(indice, 1)[0]
+        ctx.db.livros = ctx.db.livros.filter (livro => {
+            const remover = livro.autor === args.id
+            ctx.db.comentarios = ctx.db.comentarios.filter(c => remover && c.livro !== livro.id)
+            return !remover
+        })
+        ctx.db.comentarios = ctx.db.comentarios.filter(c => c.autor !== args.id)
+        return removido
+        
+    },
+    inserirLivro (parent, args, ctx, info){
+        const autorExiste = ctx.db.pessoas.some (p => p.id === args.livro.autor)
+        if (!autorExiste)
+            throw new Error ("Autor não existe")
+        const livro = {
+            id: uuidv4(),
+            titulo: args.livro.titulo,
+            edicao: args.livro.edicao,
+            autor: args.livro.autor
+        }
+        ctx.db.livros.push(livro)
+        return livro
+    },
+    removerLivro (parent, args, ctx, info){
+        const indice = ctx.db.livros.findIndex(livro => livro.id === args.id)
+        if (indice < 0)
+            throw new Error ("Livro não existe")
+        const removido = ctx.db.livros.splice (indice, 1)[0]
+        ctx.db.comentarios = ctx.db.comentarios.filter (c => c.livro !== args.id)
+        return removido
+    },
+    inserirComentario (parent, args, ctx, info){
+        const pessoaExiste = ctx.db.pessoas.some(p => p.id === args.comentario.autor)
+        const livroExiste = livros.some(l => l.id === args.comentario.livro)
+        if (!pessoaExiste || !livroExiste)
+            throw new Error ("Autor e/ou Livro inexistente(s)")
+        const comentario = {
+            id: uuidv4(),
+            texto: args.comentario.texto,
+            nota: args.comentario.nota,
+            livro: args.comentario.livro,
+            autor: args.comentario.autor
+        }
+        ctx.db.comentarios.push(comentario)
+        return comentario
+    },
+    removerComentario (parent, args, ctx, info){
+        const indice = ctx.db.comentarios.findIndex(c => c.id === args.id)
+        if (indice < 0)
+            throw new Error ("Comentário não existe")
+        return ctx.db.comentarios.splice (indice, 1)[0]
+    }
+}
+
+export default Mutation
